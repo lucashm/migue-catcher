ada9116592a11be42ef8877adf45db2bb725e9b0
Lucas Malta <lucas50xd@hotmail.com>
2017-11-01 14:49:04 -0200

    making search by email and less hard coding

--- src/index.js
+++ src/index.js
@@ -3,35 +3,36 @@ import axios from 'axios';
 
 let studentsList = [
     //merenda+
+    // {
+    //     userName: "gabrielademoraes",
+    //     score: 0,
+    //     email: "gabrielademoraes10@gmail.com"
+    // },
     {
         userName: "kahcosta",
         score: 0,
-        name: "kamilla"
-    },
-    {
-        userName: "gabrielademoraes",
-        score: 0,
-        name: "gabriela"
+        email: "kamillacosta.unb@gmail.com"
+
     },
     {
         userName: "lucaslermen",
         score: 0,
-        name: "lucas lermen"
+        email: "lucas.arthur.lermen@gmail.com"
     },
     {
         userName: "lucaspenido",
         score: 0,
-        name: "lucas penido"
+        email: "lpenidoa@me.com"
     },
     {
         userName: "mlfaa",
         score: 0,
-        name: "maria luiza"
+        email: "mlfaa@hotmail.com"
     },
     {
         userName: "miguel-alves",
         score: 0,
-        name: "miguel"
+        email: "miguelhenriquealvesdeoliveira@gmail.com"
     }
 ]
 
@@ -44,7 +45,7 @@ let repos = [
 const getCommits = () => {
     const callback = (response) => {
         if (response.data.length === 0) {
-            createList();
+            updateList();
         } else {
             pageCounter++;
             filterMessages(response);
@@ -68,19 +69,15 @@ const getInfo = (callback, pageNumber, repo) => {
 }
 
 
-const createList = () => {
-    let element = document.createElement("div");
-    let list = document.createElement("ul");
+const updateList = () => {
+    let list = document.getElementById("mainList");
     let sortedStudentsList = studentsList.sort(function (a, b) { return b.score - a.score });
     // console.log(sortedStudentsList);
     for (let student in sortedStudentsList) {
-        console.log(studentsList[student]);
-        let single = document.createElement("li");
-        single.innerHTML = sortedStudentsList[student].userName + " - " + sortedStudentsList[student].score;
-        list.appendChild(single);
+        let single = document.getElementById(student);
+        single.innerHTML = sortedStudentsList[student].userName
+            + " - " + sortedStudentsList[student].score + " signed-off's";
     }
-    element.appendChild(list);
-    document.body.appendChild(element);
 }
 
 const filterMessages = (response) => {
@@ -90,18 +87,17 @@ const filterMessages = (response) => {
         if (commitMessage.toLowerCase().includes("signed-off-by:")) {
             let count = 0;
             let hasFound = false;
-            for (let i = 0; i < commitMessage.length; i++) {
-                if (commitMessage[i] == ":") {
-                    count = i + 2;
+            for (let k = 0; k < commitMessage.length; k++) {
+                if (commitMessage[k] == "<") {
+                    count = k + 1;
                     let otherCount = count;
-                    while (commitMessage[otherCount] != " ") {
+                    while (commitMessage[otherCount] != ">") {
                         otherCount++;
                     }
-                    let signAuthor = commitMessage.substring(count, otherCount).toString().toLowerCase();
-                    for (let i = 0; i < studentsList.length; i++) {
-                        if (studentsList[i].userName === signAuthor || studentsList[i].name === signAuthor) {
-                            studentsList[i].score++;
-                            break;
+                    let signedEmail = commitMessage.substring(count, otherCount);
+                    for (let j = 0; j < studentsList.length; j++) {
+                        if (studentsList[j].email.toString().toLowerCase() == signedEmail.toString().toLowerCase()) {
+                            studentsList[j].score++;
                         }
                     }
                 }
@@ -113,4 +109,72 @@ const filterMessages = (response) => {
 }
 
 
-window.onload = getCommits();
+const startButton = () => {
+    let button = document.createElement("button")
+    button.innerHTML = "Start!";
+    button.onclick = () => getCommits();
+    return button;
+}
+
+const baseList = () => {
+    let element = document.createElement("div");
+    let list = document.createElement("ul");
+    list.id = "mainList";
+    for (let student in studentsList) {
+        let single = document.createElement("li");
+        single.id = student;
+        single.innerHTML = studentsList[student].userName
+            + " - " + studentsList[student].score + " signed-off's";
+        list.appendChild(single);
+    }
+    element.appendChild(list);
+    document.body.appendChild(element);
+
+    return element;
+}
+
+const consoleForm = () => {
+    let form = document.createElement("div");
+    let username = document.createElement("input");
+    username.id = "username";
+    username.placeholder = "username";
+    let email = document.createElement("input");
+    email.id = "email";
+    email.placeholder = "email";
+    let btn = document.createElement("button");
+    btn.innerHTML = "send!";
+    btn.onclick = () => newRegister(username.value, email.value);
+
+    form.appendChild(username);
+    form.appendChild(email);
+    form.appendChild(btn);
+
+    return form;
+}
+
+const newRegister = (username, email) => {
+    let toAppend = {
+        userName: username,
+        score: 0,
+        email: email
+    }
+
+    addOne(toAppend);
+}
+
+const addOne = (toAppend) => {
+    studentsList.push(toAppend);
+    let list = document.getElementById("mainList");
+    let newElement = document.createElement("li");
+    newElement.id = studentsList.length - 1;
+    newElement.innerHTML = toAppend.userName
+        + " - " + toAppend.score + " signed-off's";
+    list.appendChild(newElement);
+}
+
+document.body.appendChild(startButton());
+document.body.appendChild(baseList());
+document.body.appendChild(consoleForm());
+// window.onload = getCommits();
+
+
